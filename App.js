9afbe0359887763b0e00b1994ae0909851d5a73d
Hannu Karjalainen <hannu.p.karjalainen@gmail.com>
2023-09-02 11:10:33 +0300

    Second commit

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+
 
 export default function App() {
+
+  const [age, setAge] = useState('')
+  const [bmi, setBmi] = useState(0)
+
+  const calculation = () => {
+    const result
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput
+        placeholder='Click here to enter your age for calculation' />
+      <Text>Heart rate limits:</Text>
+      <Text>{bmi}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
