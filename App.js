a86d9ca8b21e4939a4e1491f8eaba46e03ee060d
Hannu Karjalainen <hannu.p.karjalainen@gmail.com>
2023-09-02 13:31:09 +0300

    Works ok, in case of input is incorrect, there is a hyphen which still needs removing

--- App.js
+++ App.js
@@ -2,24 +2,42 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-
-
 export default function App() {
+  
+  const [lower, setLower] = useState(0)
+  const [upper, setUpper] = useState(0)
 
-  const [age, setAge] = useState('')
-  const [bmi, setBmi] = useState(0)
-
-  const calculation = () => {
-    const result
+  const calculation = (text) => {
+    if (text === '') {
+      // Handle the case where the input is empty, e.g., set both lower and upper to empty strings or any default values you prefer.
+      setLower(0);
+      setUpper(0);
+      return;
+    }
+  
+    const ageValue = parseFloat(text);
+  
+    if (!isNaN(ageValue)) {
+      const lowerResult = (220 - ageValue) * 0.65;
+      const upperResult = (220 - ageValue) * 0.85;
+      setLower(lowerResult.toFixed(0));
+      setUpper(upperResult.toFixed(0));
+    } else {
+      // Handle invalid input here
+      setLower('Please, give your age in numbers');
+      setUpper('')
+    }
   }
   return (
     <View style={styles.container}>
       <Text>Age</Text>
       <TextInput
-        placeholder='Click here to enter your age for calculation' />
+        placeholder='Click here to enter your age for calculation'
+        keyboardType='decimal-pad'
+        onChangeText={text => calculation(text)}
+      />
       <Text>Heart rate limits:</Text>
-      <Text>{bmi}</Text>
-
+      <Text>{lower} - {upper}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -29,7 +47,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
   },
 });
